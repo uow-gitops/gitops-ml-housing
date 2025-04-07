c5e691a6f02b353f305c2e930cece4f9432a202b
ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2025-04-07 21:36:18 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,6 +14,10 @@ model = pickle.load(open('model/model.pkl', 'rb'))
 def home():
     return render_template("index.html")
 
+@app.route("/health")
+async health_check()():
+     return {"status": "healthy"}
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
 
