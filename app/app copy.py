3eaaf2cc855b2aa0385c6e97c6ffec2efec6e1e0
Farhan Javid <farhanjavid@Farhans-MacBook-Air.local>
2025-04-11 20:01:28 +1000

    testing action

--- app/app copy.py
+++ app/app copy.py
@@ -19,8 +19,3 @@ def main():
 
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    
-    return render_template('index.html', prediction_text ='The estimate price is :{}'.format(prediction))
-if __name__ == '__main__':  
-   app.run(debug = True)
